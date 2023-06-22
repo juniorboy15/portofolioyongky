bd0d5d20069439cc40e010487a11a75d8ff48dc7
juniorboy15 <137381734+juniorboy15@users.noreply.github.com>
2023-06-22 14:08:17 +0700

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Halo, perkenalkan saya Yongky Hidayatulloh sebagai seorang Fresh Graduate dari jurusan Teknik Komputer dan Jaringan, Saya lahir di Boyolali Tanggal 15 Mei 2005. Saya baru saja menyelesaikan program studi di jurusan Teknik Komputer dan Jaringan, dimana saya mendapatkan pemahaman yang kuat tentang Infrastuktur Jaringan, Keamanan Jaringan, dan Administrasi System. <br> <br>Sebagai seorang Fresh Graduate Saya memiliki Pengalaman Kerja sebagai siswa PKL selama 3 bulan, Teknisi Wifi di Badan Usaha FTTH selama 6 bulan, Dan Sebagai seorang freelancer dengan berbagai keahlian yang luas, saya memiliki pengetahuan yang mendalam dalam Grapic Designer, Content Creator, Front-end Developer, dan Digital Marketing. Saya memiliki kemampuan untuk mengintegrasikan keempat bidang ini dan memberikan solusi yang efektif untuk klien saya."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
